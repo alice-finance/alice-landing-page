33609575a3347d27e957d9c47eb87aae3a65a4bf
YoonjaeYoo <ryunzae@gmail.com>
2019-04-09 14:14:51 +0900

    Update Features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,94 +1,95 @@
-import React, { Component } from "react";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-class Features extends Component {
-    public render() {
-        return (
-            <section id="features" className="features__section content-section">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-xl-5 col-lg-6 order-2 order-lg-1">
-                            <div className="features_wrapper">
-                                <Feature1 />
-                                <Feature2 />
-                                <Feature3 />
-                            </div>
-                        </div>
-                        <div className="col-xl-7 col-lg-6 col-sm-8 mx-auto order-1 order-lg-2 mb-8 mb-lg-0">
-                            <div className="features_device__bg_wrapper">
-                                <img
-                                    src="assets/img/features/features__bg.png"
-                                    className="features_device__bg"
-                                    alt="Device Background"
-                                    data-animation="true"
-                                />
-                            </div>
-                            <img src="assets/img/features/features.png" className="features_device" alt="Apps" />
+const Features = () => {
+    const { i18n } = useTranslation("header");
+    return (
+        <section id="features" className="features__section content-section">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-xl-5 col-lg-6 order-2 order-lg-1">
+                        <div className="features_wrapper">
+                            <Feature1 />
+                            <Feature2 />
+                            <Feature3 />
                         </div>
                     </div>
+                    <div className="col-xl-7 col-lg-6 col-sm-8 mx-auto order-1 order-lg-2 mt-4 mb-lg-0">
+                        <div className="features_device__bg_wrapper">
+                            <img
+                                src="assets/img/features/features__bg.png"
+                                className="features_device__bg"
+                                alt="Device Background"
+                                data-animation="true"
+                            />
+                        </div>
+                        <img
+                            src={`assets/img/app/device-hero-${i18n.language.split("-")[0]}.png`}
+                            className="features_device"
+                            alt="Apps"
+                        />
+                    </div>
                 </div>
-            </section>
-        );
-    }
-}
+            </div>
+        </section>
+    );
+};
 
-const Feature1 = () => (
-    <div className="features__item-wrapper">
-        <div className="features__item">
-            <div className="mb-6 mb-lg-0 mr-lg-6">
-                <div className="features_img_wrapper">
-                    <img src="assets/img/icon/speed.png" alt="Speed" />
+const Feature1 = () => {
+    const { t } = useTranslation("features");
+    return (
+        <div className="features__item-wrapper">
+            <div className="features__item">
+                <div className="mb-6 mb-lg-0 mr-lg-6">
+                    <div className="features_img_wrapper">
+                        <img src="assets/img/icon/decentralized.png" alt="Speed" />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <h4 className="features__title">Seamless integration</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div>
-                <hr className="features__hr" />
-                <div data-toggle="tooltip" data-placement="top" title="Seamless integration" />
+                <div>
+                    <h4 className="features__title">{t("feature1.title")}</h4>
+                    <p>{t("feature1.desc")}</p>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
-const Feature2 = () => (
-    <div className="features__item-wrapper">
-        <div className="features__item">
-            <div className="mb-6 mb-lg-0 mr-lg-6">
-                <div className="features_img_wrapper">
-                    <img src="assets/img/icon/resp.png" alt="Speed" />
+const Feature2 = () => {
+    const { t } = useTranslation("features");
+    return (
+        <div className="features__item-wrapper">
+            <div className="features__item">
+                <div className="mb-6 mb-lg-0 mr-lg-6">
+                    <div className="features_img_wrapper">
+                        <img src="assets/img/icon/intuitive.png" alt="Speed" />
+                    </div>
+                </div>
+                <div>
+                    <h4 className="features__title">{t("feature2.title")}</h4>
+                    <p>{t("feature2.desc")}</p>
                 </div>
             </div>
-            <div>
-                <h4 className="features__title">Ease of access</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div>
-                <hr className="features__hr" />
-                <div data-toggle="tooltip" data-placement="top" title="Ease of access" />
-            </div>
         </div>
-    </div>
-);
+    );
+};
 
-const Feature3 = () => (
-    <div className="features__item-wrapper">
-        <div className="features__item">
-            <div className="mb-6 mb-lg-0 mr-lg-6">
-                <div className="features_img_wrapper">
-                    <img src="assets/img/icon/sett.png" alt="Speed" />
+const Feature3 = () => {
+    const { t } = useTranslation("features");
+    return (
+        <div className="features__item-wrapper">
+            <div className="features__item">
+                <div className="mb-6 mb-lg-0 mr-lg-6">
+                    <div className="features_img_wrapper">
+                        <img src="assets/img/icon/speed.png" alt="Speed" />
+                    </div>
+                </div>
+                <div>
+                    <h4 className="features__title">{t("feature3.title")}</h4>
+                    <p>{t("feature3.desc")}</p>
                 </div>
             </div>
-            <div>
-                <h4 className="features__title">Multiple services</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div>
-                <hr className="features__hr" />
-                <div data-toggle="tooltip" data-placement="top" title="Multiple services" />
-            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Features;
