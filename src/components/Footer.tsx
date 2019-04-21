428058c20eb19f39978760323a8b5a1641c82258
YoonjaeYoo <ryunzae@gmail.com>
2019-04-22 06:08:42 +0900

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,72 +1,75 @@
-import React, { Component } from "react";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-class Footer extends Component {
-    public render() {
-        return (
-            <footer className="footer footer-default">
-                <div className="container">
-                    <div className="row gutters-y">
-                        <div className="col-lg-2">
-                            <a href="#" className="logo">
-                                <img src="assets/img/logo-light.png" alt="Logo" className="logo-light" />
+const Footer = () => {
+    const { t } = useTranslation("footer");
+    return (
+        <footer className="footer footer-default">
+            <div className="container">
+                <div className="row gutters-y">
+                    <div className="col-lg-2">
+                        <a href="#" className="logo">
+                            <img src="assets/img/logo-light.png" alt="Logo" className="logo-light" />
+                        </a>
+                    </div>
+                    <div className="col-lg-4" />
+                    <div className="col-6 col-lg-2">
+                        <h6 className="nav-title mb-4">Home</h6>
+                        <div className="nav flex-column">
+                            <a className="nav-link" href={"#feautres"}>
+                                {t("features")}
+                            </a>
+                            <a className="nav-link" href={"#savings"}>
+                                {t("savings")}
+                            </a>
+                            <a className="nav-link" href={"#loan"}>
+                                {t("loan")}
+                            </a>
+                            <a className="nav-link" href={"#exchange"}>
+                                {t("exchange")}
+                            </a>
+                            <a className="nav-link" href={"#registration"}>
+                                {t("registration")}
                             </a>
                         </div>
-                        <div className="col-lg-4" />
-                        <div className="col-6 col-lg-2">
-                            <h6 className="nav-title mb-4">Home</h6>
-                            <div className="nav flex-column">
-                                <a className="nav-link" href={"#feautres"}>
-                                    Features
-                                </a>
-                                <a className="nav-link" href={"#savings"}>
-                                    Savings
-                                </a>
-                                <a className="nav-link" href={"#loan"}>
-                                    Loan
-                                </a>
-                                <a className="nav-link" href={"#exchange"}>
-                                    Exchange
-                                </a>
-                                <a className="nav-link" href={"#registration"}>
-                                    Registration
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-6 col-lg-2">
-                            <h6 className="nav-title mb-4">More Info</h6>
-                        </div>
-                        <div className="col-lg-2">
-                            <div className="social-buttons text-lg-right lead-1">
-                                <a className="social-telegram" href="https://t.me/alicenetwork" target="_blank">
-                                    <i className="fab fa-telegram" />
-                                </a>
-                                <a className="social-github" href="https://github.com/alice-network" target="_blank">
-                                    <i className="fab fa-github" />
-                                </a>
-                                <a className="social-medium" href="https://medium.com/alice-network" target="_blank">
-                                    <i className="fab fa-medium" />
-                                </a>
-                            </div>
-                        </div>
                     </div>
-                    <div className="row gutters-y mt-2 mt-lg-4 small">
-                        <div className="col-sm-9 col-lg-8">
-                            <span>© 2019 DNext Inc.</span>
-                        </div>
-                        <div className="col-sm-3 col-lg-4 d-flex align-items-center justify-content-sm-end">
-                            <ul className="nav nav-small">
-                                <li>
-                                    <a href="mailto:support@dnext.co" className="nav-link">
-                                        support@dnext.co
-                                    </a>
-                                </li>
-                            </ul>
+                    <div className="col-6 col-lg-2">
+                        <h6 className="nav-title mb-4">More Info</h6>
+                        <a className="nav-link" href={"#"}>
+                            {t("moreToCome")}
+                        </a>
+                    </div>
+                    <div className="col-lg-2">
+                        <div className="social-buttons text-lg-right lead-1">
+                            <a className="social-telegram" href="https://t.me/alicenetwork" target="_blank">
+                                <i className="fab fa-telegram" />
+                            </a>
+                            <a className="social-github" href="https://github.com/alice-network" target="_blank">
+                                <i className="fab fa-github" />
+                            </a>
+                            <a className="social-medium" href="https://medium.com/alice-network" target="_blank">
+                                <i className="fab fa-medium" />
+                            </a>
                         </div>
                     </div>
                 </div>
-            </footer>
-        );
-    }
-}
+                <div className="row gutters-y mt-2 mt-lg-4 small">
+                    <div className="col-sm-9 col-lg-8">
+                        <span>© 2019 DNext Inc.</span>
+                    </div>
+                    <div className="col-sm-3 col-lg-4 d-flex align-items-center justify-content-sm-end">
+                        <ul className="nav nav-small">
+                            <li>
+                                <a href="mailto:support@dnext.co" className="nav-link">
+                                    support@dnext.co
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
 export default Footer;
