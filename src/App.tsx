edc4608921f8ea8f5751c979c4f82c6f94729c66
YoonjaeYoo <ryunzae@gmail.com>
2019-05-03 17:11:16 +0900

    Update Header images

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import "./App.css";
 import ContentFirst from "./components/ContentFirst";
 import ContentSecond from "./components/ContentSecond";
 import ContentThird from "./components/ContentThird";
-import DownloadApp from "./components/DownloadApp";
-import Faq from "./components/Faq";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
-import Services from "./components/Services";
 import Registration from "./components/Registration";
+import Services from "./components/Services";
 
 const App = () => (
     <Fragment>
