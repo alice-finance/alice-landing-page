38118ac51ce868417ca36e1bd70e913f9fc67777
Yoonjae Yoo <ryunzae@gmail.com>
2019-08-16 16:34:21 +0900

    Update texts and images

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,12 @@ import ContentFirst from "./components/ContentFirst";
 import ContentSecond from "./components/ContentSecond";
 import ContentThird from "./components/ContentThird";
 import DownloadApp from "./components/DownloadApp";
+import EmailInput from "./components/EmailInput";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import Registration from "./components/Registration";
 import Services from "./components/Services";
 ReactGA.initialize("UA-145462592-1");
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
                             <img src="assets/img/logo-light.png" alt="Logo" className="logo-light" />
                         </a>
                     </div>
-                    <div className="col-lg-4" />
+                    <div className="col-lg-2" />
                     <div className="col-6 col-lg-2">
                         <h6 className="nav-title mb-4">Home</h6>
                         <div className="nav flex-column">
@@ -42,18 +42,33 @@ const Footer = () => {
                             <a className="nav-link" target="_blank" href={t("blog:roadmap.url")}>
                                 {t("blog:roadmap.title")}
                             </a>
+                            <a className="nav-link" target="_blank" href={t("blog:lending.url")}>
+                                {t("blog:lending.title")}
+                            </a>
+                            <a className="nav-link" target="_blank" href={t("blog:ifo.url")}>
+                                {t("blog:ifo.title")}
+                            </a>
                         </div>
                     </div>
-                    <div className="col-lg-2">
+                    <div className="col-lg-4">
                         <div className="social-buttons text-lg-right lead-1">
-                            <a className="social-github" href="https://github.com/alice-finance" target="_blank">
-                                <i className="fab fa-github" />
-                            </a>
                             <a className="social-medium" href={t("mediumUrl")} target="_blank">
                                 <i className="fab fa-medium" />
                             </a>
                             <a className="social-telegram" href={t("telegramUrl")} target="_blank">
                                 <i className="fab fa-telegram" />
+                            </a>
+                            <a className="social-twitter" href="https://twitter.com/AliceFinance" target="_blank">
+                                <i className="fab fa-twitter" />
+                            </a>
+                            <a className="social-facebook" href="https://facebook.com/alicedotfinance" target="_blank">
+                                <i className="fab fa-facebook" />
+                            </a>
+                            <a className="social-reddit" href="https://reddit.com/r/AliceFinance" target="_blank">
+                                <i className="fab fa-reddit" />
+                            </a>
+                            <a className="social-github" href="https://github.com/alice-finance" target="_blank">
+                                <i className="fab fa-github" />
                             </a>
                         </div>
                     </div>
