d72ccd2b9ab73f8ed8d8d5a45efedd2ce97cbb14
Yoonjae Yoo <ryunzae@gmail.com>
2019-09-11 13:53:21 +0900

    Support español

--- src/i18n/index.tsx
+++ src/i18n/index.tsx
@@ -3,9 +3,10 @@ import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./locales/en.json";
+import es from "./locales/es.json";
 import ko from "./locales/ko.json";
 
-const resources = { en, ko };
+const resources = { en, es, ko };
 
 i18n.use(initReactI18next)
     .use(LanguageDetector)
